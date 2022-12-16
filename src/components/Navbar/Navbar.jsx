d6aaa429cea6c31f79645b88bbac0eb6c8022d43
techx1 <ss.uitech01@gmail.com>
2022-12-17 00:30:50 +0500

    navbar done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,20 @@
+import Link from 'next/link';
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='flex space-x-6 mx-auto w-[50%]'>
+      <Link href={''}>
+        <p className='uppercase font-bold tracking-tighter'>About</p>
+      </Link>
+      <Link href={''}>
+        <p className='uppercase font-bold tracking-thick'>Start a campaign</p>
+      </Link>
+      <Link href={''}>
+        <p className='uppercase font-bold tracking-thick'>River Dao</p>
+      </Link>
+      <Link href={''}>
+        <p className='uppercase font-bold tracking-thick'>Contact</p>
+      </Link>
+    </div>
+  );
 };
